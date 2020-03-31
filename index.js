7aa7abc82e9b285e970c135e7d0aa15c8a9a31fe
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-03-31 10:41:02 +0700

    Initiate main structure

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ class todo {
     this.text = newText
   }
 }
+/* when it's clicked, insert new input element before the new-todo-container */
+const newInputElement = document.createElement("input")
+newInputElement.setAttribute("type", "text")
+const newTodoContainer = document.getElementById("new-todo-container")
+const todoContainer = document.getElementById("todo-container")
+newTodoContainer.addEventListener("click", function() {
+  todoContainer.insertBefore(newInputElement, this)
+})
