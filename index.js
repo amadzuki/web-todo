cea4ce02e6a9663ce15477983d0ebb2568e279f6
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-04-01 17:55:42 +0700

    create new input for todo

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ class todo {
   }
 }
 /* when it's clicked, insert new input element before the new-todo-container */
-const newInputElement = document.createElement("input")
-newInputElement.setAttribute("type", "text")
+
 const newTodoContainer = document.getElementById("new-todo-container")
-const todoContainer = document.getElementById("todo-container")
-newTodoContainer.addEventListener("click", function() {
-  todoContainer.insertBefore(newInputElement, this)
-})
+const setNewInputBox = function() {
+  const newInputElement = document.createElement("div")
+  newInputElement.setAttribute("class", "")
+  randomID = Math.random() //just to test
+  newInputElement.innerHTML = `<input type="text" data-id="${randomID}">`
+
+  this.before(newInputElement, this)
+}
+newTodoContainer.addEventListener("click", setNewInputBox)
