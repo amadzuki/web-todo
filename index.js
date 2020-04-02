b1fa2da1df75e47c14a3262a925b1d05cf6bc347
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-04-02 22:02:11 +0700

    Handle saving data and display it

--- index.js
+++ index.js
@@ -1,21 +1,80 @@
-class todo {
-  constructor(text) {
-    this.text
+class Todo {
+  constructor(objectConstructor) {
+    Object.assign(this, objectConstructor)
   }
 
   setNewText(newText) {
     this.text = newText
   }
+
+  setDueDate = date => {
+    this.dueDate = date
+  }
+
+  isFavorite = boolean => {
+    if (boolean) {
+      this.favorite = boolean
+    } else {
+      delete this.favorite
+    }
+  }
 }
-/* when it's clicked, insert new input element before the new-todo-container */
 
+// FUNCTIONS ----------------------------------------------------------
+
+const saveToStorage = () => {
+  localStorage.setItem("allTodosData", JSON.stringify(allTodos))
+}
+const setNewElement = tagName => document.createElement(tagName)
+
+// display data from storage
+const displayList = arrayList => {
+  arrayList.map(todo => {
+    const newListContainer = setNewElement("div")
+    newListContainer.setAttribute("data-id", todo.id)
+    newListContainer.innerHTML = `<div class="check-bullet"></div><div>${todo.text}</div>`
+    todoContainer.insertBefore(newListContainer, newTodoContainer)
+  })
+}
+//---------------------------------------------------------------------
+
+// useful assignment
+const todoContainer = document.getElementById("todo-container")
 const newTodoContainer = document.getElementById("new-todo-container")
-const setNewInputBox = function() {
-  const newInputElement = document.createElement("div")
-  newInputElement.setAttribute("class", "")
-  randomID = Math.random() //just to test
-  newInputElement.innerHTML = `<input type="text" data-id="${randomID}">`
 
-  this.before(newInputElement, this)
+// INITIATE
+let allTodos = []
+if (localStorage.getItem("allTodosData") !== null) {
+  allTodosData = JSON.parse(localStorage.getItem("allTodosData"))
+  allTodos = allTodosData.map(object => new Todo(object))
+  displayList(allTodos)
+}
+
+const newID = () => {
+  if (allTodos.length === 0) {
+    firstID = 1
+    return firstID
+  }
+  const lastID = allTodos.reduce((max, todo) => Math.max(max, todo.id), 0)
+  const newIDValue = lastID + 1
+  return newIDValue
+}
+
+/* insert new input element on click */
+const setNewInputBox = function() {
+  newInputElement = setNewElement("div")
+  newInputElement.setAttribute("class", "todo-list")
+  newInputElement.innerHTML = `<input type="text" onfocusout="getText(this)">`
+  newTodoContainer.before(newInputElement, newTodoContainer)
+  newInputElement.firstChild.focus()
 }
 newTodoContainer.addEventListener("click", setNewInputBox)
+
+// Add the text into array of objects
+const getText = textBox => {
+  const idValue = newID()
+  const newTodo = { id: idValue, text: textBox.value }
+  allTodos.push(new Todo(newTodo))
+  textBox.parentNode.dataset.id = idValue
+  saveToStorage()
+}
