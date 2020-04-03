160a867f979989623f373bd78d9269c4726b81c1
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-04-04 00:26:14 +0700

    Handle seeding

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import seed from "./seed"
+
 class Todo {
   constructor(objectConstructor) {
     Object.assign(this, objectConstructor)
---
+++ seed.js
@@ -0,0 +1,35 @@
+export default function(array) {
+  array = [
+    {
+      id: 1,
+      text: "kill",
+      favorite: true
+    },
+    {
+      id: 2,
+      text: "chill"
+    },
+    {
+      id: 3,
+      text: "ngemill"
+    },
+    {
+      id: 4,
+      text: "Laolao"
+    },
+    {
+      id: 5,
+      text: "Berjibaku",
+      completed: true
+    },
+    {
+      id: 6,
+      text: "coding",
+      favorite: true
+    },
+    {
+      id: 7,
+      text: "Ngopi"
+    }
+  ]
+}
